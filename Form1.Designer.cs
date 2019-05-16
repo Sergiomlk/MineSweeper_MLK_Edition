229e64c08c3fe784a82dc0529cfa104c433ed1d3
35124 <550688774@qq.com>
2019-05-17 00:42:31 +0800

    Sergiomlk

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MineSweeper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            //this.button1 = new System.Windows.Forms.Button();
+            //this.SuspendLayout();
+            // 
+            // button1
+            // 
+            //this.button1.Location = new System.Drawing.Point(359, 12);
+            //this.button1.Name = "button1";
+            //this.button1.Size = new System.Drawing.Size(75, 23);
+            //this.button1.TabIndex = 0;
+            //this.button1.Text = "button1";
+            //this.button1.UseVisualStyleBackColor = true;
+            //this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 550);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+            for (int i = 0; i < 100; i++)
+            {
+                buttons[i] = new _Button();
+                buttons[i].TabIndex = i;
+                buttons[i].Width = 50;
+                buttons[i].Height = 50;
+                buttons[i].Text = " ";
+                buttons[i].Left = Left + 25;
+                buttons[i].Top = Top + 25;
+                //buttons[i].MouseDoubleClick += new MouseEventHandler(ButtonDoubleClick);
+                buttons[i].MouseDown += new MouseEventHandler(ButtonSingleClick);
+                this.Controls.Add(buttons[i]);
+                Left += 50;
+                if (Left >= 500)
+                {
+                    Left = 0;
+                    Top += 50;
+                }
+            }
+        }
+        #endregion
+        _Button[] buttons = new _Button[100];
+
+        class _Button : Button
+        {
+            public bool IsClicked { get; set; } = false;
+            public bool ContainsMine { get; set; } = false;
+            public int AroundNumber { get; set; } = 0;
+            public Enumbuttonstatus Status{ get; set; } = Enumbuttonstatus.Unopened;
+            //0--未翻开
+            //1--已翻开
+            //2--红旗标记
+            //3--问号标记
+        }
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MineSweeper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        enum Enumbuttonstatus
+        {
+            Opened,      //被翻开
+            Unopened,    //未翻开
+            Marked,      //红旗标记
+            Suspected    //问号标记
+        }
+
+        private bool Initialized = false;
+        HashSet<int> MinesPosition = new HashSet<int>();
+        private int UnopenedButtons = 100;
+        Bitmap[] bitmaps = new Bitmap[]
+        {
+            new Bitmap("？.png"),
+            new Bitmap("1.png"),
+            new Bitmap("2.png"),
+            new Bitmap("3.png"),
+            new Bitmap("4.png"),
+            new Bitmap("5.png"),
+            new Bitmap("6.png"),
+            new Bitmap("7.png"),
+            new Bitmap("8.png"),
+            new Bitmap("boom.png"),
+            new Bitmap("flag.png"),
+            new Bitmap("empty.png"),
+            new Bitmap("opened.png")
+        };
+
+        private void ButtonDoubleClick(object sender, MouseEventArgs e)
+        {
+            Button button = sender as Button;
+            MessageBox.Show(button.TabIndex.ToString());
+        }
+
+        private void MouseLeftButtonClick(object sender, MouseEventArgs e)
+        {
+            _Button CurrentButton = sender as _Button;
+            if (CurrentButton.IsClicked) return;
+            int Position = CurrentButton.TabIndex;
+            if (!Initialized)
+            {
+                InitMinePanel(Position);
+                Initialized = true;
+            }
+            if (buttons[Position].ContainsMine)
+            {
+                foreach (var i in MinesPosition)
+                {
+                    buttons[i].Image = bitmaps[9];
+                }
+                MessageBox.Show("游戏结束！");
+                Application.Exit();
+            }
+            else
+            {
+                if (buttons[Position].AroundNumber != 0)
+                {
+                    buttons[Position].Image = bitmaps[buttons[Position].AroundNumber];
+                    buttons[Position].IsClicked = true;
+                    buttons[Position].Status = Enumbuttonstatus.Opened;
+                    //buttons[Position].Image = bitmaps[12];
+                    --UnopenedButtons;
+                }
+                else if (buttons[Position].AroundNumber == 0)
+                {
+                    buttons[Position].Enabled = false;
+                    buttons[Position].IsClicked = true;
+                    buttons[Position].Status = Enumbuttonstatus.Opened;
+                    --UnopenedButtons;
+                    BreadthFirstSearch(Position);
+                }
+            }
+        }
+
+        //广度遍历算法
+        private void BreadthFirstSearch(int Position)
+        {
+            Queue<int> AuxiliaryQueue = new Queue<int>();
+            AuxiliaryQueue.Enqueue(Position);
+            while (!(AuxiliaryQueue.Count == 0))
+            {
+                int QueueFront = AuxiliaryQueue.First();
+                AuxiliaryQueue.Dequeue();
+                foreach (var Around in GetRoundbuttons(QueueFront))
+                {
+                    if (!buttons[Around].IsClicked)
+                    {
+                        if (buttons[Around].AroundNumber == 0)
+                        {
+                            buttons[Around].Enabled = false;
+                            buttons[Around].IsClicked = true;
+                            buttons[Around].Status = Enumbuttonstatus.Opened;
+                            --UnopenedButtons;
+                            AuxiliaryQueue.Enqueue(Around);
+                        }
+                        else
+                        {
+                            buttons[Around].Image = bitmaps[buttons[Around].AroundNumber];
+                            buttons[Around].IsClicked = true;
+                            buttons[Around].Status = Enumbuttonstatus.Opened;
+                            //buttons[Around].Image = bitmaps[12];
+                            --UnopenedButtons;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void MouseRightButtonClick(object sender, MouseEventArgs e)
+        {
+            _Button button = sender as _Button;
+            if (button.Status == Enumbuttonstatus.Unopened)
+            {
+                button.Status = Enumbuttonstatus.Marked;
+                button.Image = bitmaps[10];
+            }
+            else if (button.Status == Enumbuttonstatus.Marked)
+            {
+                button.Status = Enumbuttonstatus.Suspected;
+                button.Image = bitmaps[0];
+            }
+            else if (button.Status == Enumbuttonstatus.Suspected)
+            {
+                button.Status = Enumbuttonstatus.Unopened;
+                button.Image = bitmaps[11];
+            }
+            else
+            {
+
+            }
+        }
+
+        private void ButtonSingleClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left) MouseLeftButtonClick(sender, e);
+            if (e.Button == MouseButtons.Right) MouseRightButtonClick(sender, e);
+            if (UnopenedButtons == 10)
+            {
+                MessageBox.Show("成功！");
+                Application.Exit();
+            }
+        }
+
+        private void InitMinePanel(int _Exception)
+        {
+            Random random = new Random();
+
+            int i = 0;
+            while(i < 10)
+            {
+                var x = random.Next(0, 100);
+                var Around = GetRoundbuttons(x);
+                if (!MinesPosition.Contains(x) && x != _Exception && !Around.Contains(_Exception)) 
+                {
+                    MinesPosition.Add(x);
+                    buttons[x].ContainsMine = true;
+                    foreach (var AroundBlock in GetRoundbuttons(x))
+                    {
+                        ++buttons[AroundBlock].AroundNumber;
+                    }
+                    ++i;
+                }
+            }
+        }
+        private IEnumerable<int> GetRoundbuttons(int i)
+        {
+            int x = i % 10;
+            int y = i / 10;
+            if (y >= 1) 
+                yield return i - 10;
+            if (y <= 8)
+                yield return i + 10;
+            if (x <= 8)
+                yield return i + 1;
+            if (x >= 1)
+                yield return i - 1;
+            if (x >= 1 && y >= 1)
+                yield return i - 11;
+            if (x >= 1 && y <= 8)
+                yield return i + 9;
+            if (x <= 8 && y <= 8)
+                yield return i + 11;
+            if (x <= 8 && y >= 1)
+                yield return i - 9;
+        }
+    }
+}
